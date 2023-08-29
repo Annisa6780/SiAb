6bbed068539e7c2b8d971b088ce3d8c6697890bc
arahmawati606@gmail.com <ilar6780@gmail.com>
2023-08-29 12:01:20 +0700

    update front end

--- FIX/config.php
+++ FIX/config.php
@@ -1,29 +1,14 @@
-<?php
-// Sambungkan ke database MySQL
-$host = "192.168.0.153";
-$username = "username_db";
-$password = "password_db";
+<?php 
+ 
+$server = "localhost";
+$user = "root";
+$pass = "";
 $database = "test";
-
-$koneksi = new mysqli($host, $username, $password, $database);
-
-// Periksa koneksi
-if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
+ 
+$conn = mysqli_connect($server, $user, $pass, $database);
+ 
+if (!$conn) {
+    die("<script>alert('Gagal tersambung dengan database.')</script>");
 }
-
-// Ambil data dari formulir HTML
-$nama = $_POST['email'];
-$email = $_POST['password'];
-
-// Simpan data ke database
-$query = "INSERT INTO tabel_pengguna (nama, email) VALUES ('$nama', '$email')";
-if ($koneksi->query($query) === TRUE) {
-    echo "Data berhasil disimpan";
-} else {
-    echo "Error: " . $query . "<br>" . $koneksi->error;
-}
-
-// Tutup koneksi
-$koneksi->close();
+ 
 ?>
